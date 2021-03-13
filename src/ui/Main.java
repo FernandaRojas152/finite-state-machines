eadb5ce78fa694194efe72ed246aadd203d7acd9
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2021-03-13 17:00:16 -0500

    adding css file to the interface

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,71 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+
+public class Main extends Application{
+
+	private PrincipalWindowController principal;
+	private double xOffset = 0;
+	private double yOffset = 0;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PrincipalWindow.fxml"));
+			Parent root = fxmlLoader.load();
+			principal = fxmlLoader.getController();
+			root.setOnMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					xOffset = event.getSceneX();
+					yOffset = event.getSceneY();
+				}
+			});
+			root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					primaryStage.setX(event.getScreenX() - xOffset);
+					primaryStage.setY(event.getScreenY() - yOffset);
+				}
+			});
+			setPrincipal(fxmlLoader.getController());
+			Scene scene= new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("/resources/fontstyle.css").toExternalForm());
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.setScene(scene);
+			//primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("")));
+			primaryStage.setTitle("Finite states");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent arg0) {
+				}
+			});
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	public PrincipalWindowController getPrincipal() {
+		return principal;
+	}
+
+	public void setPrincipal(PrincipalWindowController principal) {
+		this.principal = principal;
 	}
 }
