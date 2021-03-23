9b9a7f41349f2754b3ca920c4d180d56a4910faf
Manuel Balanta <mbalantaq@gmail.com>
2021-03-22 20:24:30 -0500

    More attributes, methods and constructor in Automaton class

--- src/model/Automaton.java
+++ src/model/Automaton.java
@@ -2,14 +2,54 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Stack;
+import java.util.HashMap;
 /**
  * 
  * @author Fernanda, Balanta, Yimar
  *
  */
 public class Automaton {
+	
+	private String type;
 	private ArrayList<State> states;
 	private char[] stimuli;
+	private char[] outputs;
+	
+	private HashMap<State, Integer> index;
+	
+	public Automaton(String type, ArrayList<State> states, char[] stimuli, char[] outputs) {
+		
+		
+		this.type = type;
+		this.states = states;
+		this.stimuli = stimuli;
+		this.outputs = outputs;
+		index = new HashMap<>();
+		initializeIndex();
+		
+	}
+
+	private void initializeIndex(){
+		for (int i = 0; i < states.size(); i++) {
+			index.put(states.get(i), i);
+		}
+	}
+
+	public String getType(){
+		return type;
+	}
+
+	public ArrayList<State> getStates(){
+		return states;
+	}
+
+	public char[] getStimuli(){
+		return stimuli;
+	}
+
+	public char[] getOutputs(){
+		return outputs;
+	}
 	
 	public void moore() {
 		
