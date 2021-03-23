d9a94093b6ef732e5e8f89f078cb154fbe9ebb3a
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2021-03-22 20:11:28 -0500

    state class finished, fxml machine edited

--- src/model/Automaton.java
+++ src/model/Automaton.java
@@ -1,9 +1,21 @@
 package model;
 
 import java.util.ArrayList;
-
+import java.util.Stack;
+/**
+ * 
+ * @author Fernanda, Balanta, Yimar
+ *
+ */
 public class Automaton {
 	private ArrayList<State> states;
 	private char[] stimuli;
-
+	
+	public void moore() {
+		
+	}
+	
+	public void mealy() {
+		
+	}
 }
--- src/model/State.java
+++ src/model/State.java
@@ -1,5 +1,72 @@
 package model;
 
-public class State {
+import java.util.ArrayList;
 
+/**
+ * 
+ * @author Fernanda, Balanta, Yimar
+ *
+ */
+public class State {
+	private boolean visited;
+	private String name;
+	private char[] result;
+	private int current;
+	private int prevC;
+	private ArrayList<State> prevState;
+	
+	public State(String name, char[] result) {
+		this.name= name;
+		visited= false;
+		this.result= result;
+		prevState= new ArrayList<State>();
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char[] getResult() {
+		return result;
+	}
+
+	public void setResult(char[] result) {
+		this.result = result;
+	}
+
+	public int getCurrent() {
+		return current;
+	}
+
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+
+	public int getPrevC() {
+		return prevC;
+	}
+
+	public void setPrevC(int prevC) {
+		this.prevC = prevC;
+	}
+
+	public ArrayList<State> getPrevState() {
+		return prevState;
+	}
+
+	public void setPrevState(ArrayList<State> prevState) {
+		this.prevState = prevState;
+	}
 }
