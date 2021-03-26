281d2dd5fd779a08c656305dba7c9b505182db80
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2021-03-25 20:36:49 -0500

    icon added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 //import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -42,8 +43,8 @@ public class Main extends Application{
 			scene.getStylesheets().add(getClass().getResource("/resources/PrincipalWindow.css").toExternalForm());
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setScene(scene);
-			//primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("")));
-			primaryStage.setTitle("Finite states");
+			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
+			primaryStage.setTitle("Finite state machine");
 			primaryStage.setResizable(false);
 			primaryStage.show();
 			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
